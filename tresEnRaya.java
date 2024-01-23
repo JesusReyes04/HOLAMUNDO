a1e2dd99e9a411928bbd66b03e03a7b380bdecf3
JesusReyes04 <145621433+JesusReyes04@users.noreply.github.com>
2024-01-23 09:17:23 +0100

    tresEnRaya v1.2

--- tresEnRaya.java
+++ tresEnRaya.java
@@ -1,61 +1,65 @@
 import java.util.Scanner;
 
-public class App {
+public class tresEnRaya {
     public static void main(String[] args) {
         String[][] tablero = { { " * ", " * ", " * " }, { " * ", " * ", " * " }, { " * ", " * ", " * " } };
-        int turno = 0;
-        boolean esJugadorX;
+        String jugador=" X ";
+        int turno=0;
         boolean hasGanao=false;
         mostrarTablero(tablero);
-        while (turno < 9||!hasGanao) {
-            esJugadorX = comprobarturno(turno);
-            introducirCaracter(tablero, esJugadorX);
+        while (turno < 8&&!hasGanao) {
+            introducirCaracter(tablero, jugador);
             mostrarTablero(tablero);
+            hasGanao = comprobarGanador(tablero,jugador);
             turno++;
+            jugador=cambiarJugador(jugador);
         }
-
+        
     }
 
-    public static boolean comprobarturno(int turno) {
-        boolean juegaLaX;
-        if (turno % 2 == 0) {
-            System.out.println("turno del jugador 1");
-            juegaLaX = true;
-        } else {
-            System.out.println("turno del jugador 2");
-            juegaLaX = false;
+    public static boolean comprobarGanador(String[][] tablero, String jugador) {
+        boolean ganador=false;
+        for(int i=0; i<3; i++){
+            if (tablero[i][0].equals(jugador)&&tablero[i][1].equals(jugador)&&tablero[i][2].equals(jugador)) {
+                ganador=true;
+            }
+            if (tablero[0][i].equals(jugador)&&tablero[1][i].equals(jugador)&&tablero[2][i].equals(jugador)) {
+                ganador=true;
+            }
         }
-        return juegaLaX;
+        if (tablero[0][0].equals(jugador)&&tablero[1][1].equals(jugador)&&tablero[2][2].equals(jugador)) {
+            ganador=true;
+        }
+        if (tablero[2][0].equals(jugador)&&tablero[1][1].equals(jugador)&&tablero[0][2].equals(jugador)) {
+            ganador=true;
+        }
+        return ganador;
     }
 
-    public static void introducirCaracter(String[][] tablero, boolean esJugadorX) {
+    public static String cambiarJugador(String jugador) {
+        return(jugador.equals(" X "))?" O ":" X ";
+    }
+
+    public static void introducirCaracter(String[][] tablero, String jugador) {
         int coordenadaI;
         int coordenadaJ;
         Scanner leer = new Scanner(System.in);
-        String jugador;
-        jugador = (esJugadorX) ? " X " : " O ";
-        System.out.println("Introduce las coordenadas de tu jugada");
         do {
-            System.out.println("Numero de la fila (entre el 1 y el 3)");
-            coordenadaI = leer.nextInt();
-        } while (coordenadaI > 3 || coordenadaI < 1);
-        do {
-            System.out.println("Numero de la columna (entre el 1 y el 3)");
-            coordenadaJ = leer.nextInt();
-        } while (coordenadaJ > 3 || coordenadaJ < 1);
-        while (tablero[coordenadaI - 1][coordenadaJ - 1].equals(" X ") || tablero[coordenadaI - 1][coordenadaJ - 1].equals(" O ")) {
-            System.out.println("Coordenadas no válidas mete otras");
-            do {
-                System.out.println("Numero de la fila (entre el 1 y el 3)");
-                coordenadaI = leer.nextInt();
-            } while (coordenadaI > 3 || coordenadaI < 1);
-            do {
-                System.out.println("Numero de la columna (entre el 1 y el 3)");
-                coordenadaJ = leer.nextInt();
-            } while (coordenadaJ > 3 || coordenadaJ < 1);
-        }
+            coordenadaI=obtenerCoordena("Numero de la fila");
+            coordenadaJ=obtenerCoordena("Numero de la columna");
+        } while (tablero[coordenadaI - 1][coordenadaJ - 1].equals(" X ") || tablero[coordenadaI - 1][coordenadaJ - 1].equals(" O "));  
         tablero[coordenadaI - 1][coordenadaJ - 1] = jugador;
 
+    }
+    
+    public static int obtenerCoordena(String mesaje){
+        Scanner leer = new Scanner(System.in);
+        int coordenada;
+        do {
+            System.out.println(mesaje+" (entre el 1 y el 3)");
+            coordenada=leer.nextInt();
+        } while (coordenada<1||coordenada>3);
+        return coordenada;
     }
 
     public static void mostrarTablero(String[][] tablero) {
@@ -63,6 +67,7 @@ public class App {
             for (int j = 0; j < tablero[i].length; j++) {
                 System.out.print(tablero[i][j]);
             }
+            
             System.out.println("");
         }
     }
